6c8cb1034d17c341e5dfc9a34dc3da9d96c5643a
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint
    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
 
   res.end()
 })
@@ -13,12 +14,12 @@ function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
+<<<<<<< HEAD
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
-  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
-}
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+>>>>>>> 33e4157 (feat: add user-agent endpoint)
 
-server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
 
-if (require.main !== module) module.exports = server
